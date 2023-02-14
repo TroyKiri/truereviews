c2c6f1add501b16d2b520f98e3e023cb062d638f
TroyKiri <TroyKiri@users.noreply.github.com>
2023-02-14 07:52:55 +0000

    deploy: 45b90ff70021a3bdc40aab7c4afcbb4b00195797

---
+++ _next/static/utntcMy22mR6xcAptLXYP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-e3fcc6947663cb5b.js","static/css/fe38371dec715dbc.css","static/chunks/pages/index-0e270861417d1ecd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
