f89ba73e9cc1ed6ab3b9ae73e3eb88a210fda340
Kirill Troitsky <Kirill.Troitsky@leomax.ru>
2023-02-15 09:08:54 +0300

    trying to fix images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  basePath: isProd ? 'https://troykiri.github.io/truereviews/' : '',
   assetPrefix: isProd ? 'https://troykiri.github.io/truereviews/' : '',
   reactStrictMode: true,
   images: {
